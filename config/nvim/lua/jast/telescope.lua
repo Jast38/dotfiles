d702b69b855b00067d5f0f7cac1e00d6244d6bb0
jast38 <jannick.stuby@gmail.com>
2021-07-04 12:21:41 +0200

    changed nvim config

--- config/nvim/lua/jast/telescope.lua
+++ config/nvim/lua/jast/telescope.lua
@@ -31,14 +31,18 @@ require('telescope').setup {
         }
     },
     extensions = {
-        fzy_native = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
+        fzf = {
+        fuzzy = true,                    -- false will only do exact matching
+        override_generic_sorter = false, -- override the generic sorter
+        override_file_sorter = true,     -- override the file sorter
+        case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
         }
     }
 }
 
-require('telescope').load_extension('fzy_native')
+require('telescope').load_extension('coc')
+require('telescope').load_extension('fzf')
 
 local M = {}
 M.search_dotfiles = function()
@@ -49,7 +53,8 @@ M.search_dotfiles = function()
 end
 
 M.git_branches = function()
-    require("telescope.builtin").git_branches({
+    local opt = require('telescope.themes').get_dropdown()
+    require("telescope.builtin").git_branches(opt)({
         attach_mappings = function(_, map)
             map('i', '<c-d>', actions.git_delete_branch)
             map('n', '<c-d>', actions.git_delete_branch)
@@ -72,8 +77,8 @@ M.find_config_files = function()
 end
 
 M.file_browser = function()
-  telescope_builtin.file_browser {
-      hidden = 'True'
+    telescope_builtin.file_browser(require('telescope.themes').get_dropdown({})) {
+        hidden = 'True'
   }
 end
 return M
